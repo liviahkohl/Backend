891ec370e545ab4785ad4c11ad88fc4ddb6179f9
liviahkohl <analiviakohlassisdasilva@gmail.com>
2025-04-10 10:07:38 -0400

    atividades aula 01

---
+++ Variáveis e tipos de dados/exercicio 4.js
@@ -0,0 +1,4 @@
+let peso = 60;
+let altura = 1.63;
+let soma = peso / (altura * altura).toFixed(2)
+console.log(`seu imc é ${soma}`);
--- exercicio 4.js
+++
@@ -1,4 +0,0 @@
-let peso = 60;
-let altura = 1.63;
-let soma = peso / (altura * altura)
-console.log(soma)
